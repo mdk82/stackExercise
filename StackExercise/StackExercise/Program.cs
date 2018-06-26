565002786a6888cdab56c7885980612e1bb9a4c4
Michael Klingelberg <Mike@Michaels-MacBook-Air.local>
2018-06-26 11:14:05 -0700

    implementing a stack in C#

---
+++ StackExercise/StackExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace StackExercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Count);
+
+            foreach (int num in stack)
+            {
+                Console.WriteLine(num);
+            }
+
+            stack.Pop();
+
+        }
+    }
+}
---
+++ StackExercise/StackExercise/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StackExercise
+{
+    public class Stack<T>
+    {
+        private T[] numbers;
+        private int currentStackIndex;
+
+        public Stack(int Num)
+        {
+            if (Num < 0)
+                throw new ArgumentOutOfRangeException();
+
+            numbers = new T[Num];
+            currentStackIndex = 0;
+        }
+
+        public void Push(T Num)
+        {
+            if(currentStackIndex + 1 >= numbers.Length)
+            {
+                Array.Resize(ref numbers, (numbers.Length + 1) * 2);
+            }
+
+            numbers[currentStackIndex] = Num;
+        }
+
+        public T Pop()
+        {
+            if (currentStackIndex == 0)
+                throw new InvalidOperationException("Stack is empty");
+
+            T value = numbers[--currentStackIndex];
+            numbers[currentStackIndex] = default(T);
+            return value;
+        }
+    }
+}
